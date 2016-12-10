4a57ca61af59b800f9b69a5214818ade1e67f2af
HongAnhDo <dohong1803@gmail.com>
2016-12-10 07:26:28 +0700

    sửa nhiều lắm :P

--- src/GUI/StudentMain.java
+++ src/GUI/StudentMain.java
@@ -1,7 +1,9 @@
 package GUI;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import static java.lang.Thread.sleep;
 import javax.swing.ImageIcon;
 
 
@@ -13,6 +15,7 @@ public class StudentMain extends javax.swing.JFrame {
         this.invalidate();
         this.validate();
         repaint();
+        InfoThread newThread = new InfoThread();
     }
 
     @SuppressWarnings("unchecked")
@@ -31,7 +34,7 @@ public class StudentMain extends javax.swing.JFrame {
             }
         };
         pDHBK = new javax.swing.JPanel(){
-            ImageIcon icon = new ImageIcon("src/Icon/BK_2.jpg");
+            ImageIcon icon = new ImageIcon("src/Icon/ThuvienTaQuangBuu.jpg");
 
             public void paintComponent(Graphics g){
                 Dimension d = getSize();
@@ -40,40 +43,76 @@ public class StudentMain extends javax.swing.JFrame {
                 super.paintComponent(g);
             }
         };
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
         pMain = new javax.swing.JPanel();
+        jPanel1 = new javax.swing.JPanel(){
+            ImageIcon icon = new ImageIcon("123.JPG");
+
+            public void paintComponent(Graphics g){
+                Dimension d = getSize();
+                g.drawImage(icon.getImage(), 0, 0, d.width, d.height, null);
+                setOpaque(false);
+                super.paintComponent(g);
+            }
+        };
+        jLabelPM = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
         mMenu = new javax.swing.JMenuBar();
         mTrangChu = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
         mInforUser = new javax.swing.JMenu();
         mDoiMatKhau = new javax.swing.JMenuItem();
         mThongTinCaNhan = new javax.swing.JMenuItem();
         javax.swing.JMenu mSearch = new javax.swing.JMenu();
-        mTimKiemMonHoc = new javax.swing.JMenuItem();
-        mTimKiemSV = new javax.swing.JMenuItem();
+        mTimKiemSinhVien = new javax.swing.JMenuItem();
+        jMenuItem1 = new javax.swing.JMenuItem();
         jMenu1 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setBackground(new java.awt.Color(204, 255, 255));
+        setBackground(new java.awt.Color(51, 153, 255));
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         setPreferredSize(new java.awt.Dimension(1366, 768));
         getContentPane().setLayout(null);
 
         pTieuDe.setPreferredSize(new java.awt.Dimension(1366, 142));
 
-        pLogo.setBackground(new java.awt.Color(255, 102, 102));
+        pLogo.setBackground(new java.awt.Color(153, 153, 255));
         pLogo.setLayout(null);
 
         pDHBK.setBackground(new java.awt.Color(255, 255, 255));
         pDHBK.setPreferredSize(new java.awt.Dimension(1300, 0));
 
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 0, 0));
+        jLabel1.setText("Phần mềm quản lý sinh viên");
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 0, 0));
+        jLabel2.setText("Trường Đại Học Bách Khoa Hà Nội");
+
         javax.swing.GroupLayout pDHBKLayout = new javax.swing.GroupLayout(pDHBK);
         pDHBK.setLayout(pDHBKLayout);
         pDHBKLayout.setHorizontalGroup(
             pDHBKLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1256, Short.MAX_VALUE)
+            .addGroup(pDHBKLayout.createSequentialGroup()
+                .addGroup(pDHBKLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(pDHBKLayout.createSequentialGroup()
+                        .addGap(87, 87, 87)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 348, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(pDHBKLayout.createSequentialGroup()
+                        .addGap(56, 56, 56)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 424, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(776, Short.MAX_VALUE))
         );
         pDHBKLayout.setVerticalGroup(
             pDHBKLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 131, Short.MAX_VALUE)
+            .addGroup(pDHBKLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(5, 5, 5)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(34, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout pTieuDeLayout = new javax.swing.GroupLayout(pTieuDe);
@@ -87,23 +126,57 @@ public class StudentMain extends javax.swing.JFrame {
         );
         pTieuDeLayout.setVerticalGroup(
             pTieuDeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(pTieuDeLayout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addGroup(pTieuDeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(pLogo, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(pDHBK, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)))
+            .addComponent(pLogo, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(pDHBK, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         getContentPane().add(pTieuDe);
-        pTieuDe.setBounds(0, 0, 1366, 130);
+        pTieuDe.setBounds(0, 0, 1366, 108);
 
-        pMain.setBackground(new java.awt.Color(255, 255, 255));
-        pMain.setPreferredSize(new java.awt.Dimension(1366, 610));
+        pMain.setMinimumSize(new java.awt.Dimension(1368, 510));
+        pMain.setPreferredSize(new java.awt.Dimension(1366, 510));
         pMain.setLayout(null);
         getContentPane().add(pMain);
-        pMain.setBounds(0, 130, 1366, 620);
+        pMain.setBounds(0, 110, 1366, 510);
+
+        jPanel1.setBackground(new java.awt.Color(204, 204, 255));
+
+        jLabelPM.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabelPM.setForeground(new java.awt.Color(0, 51, 204));
+        jLabelPM.setText("Phần mềm quản lý sinh viên Trường Đại Học Bách Khoa Hà Nội");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jSeparator1)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(387, 387, 387)
+                .addComponent(jLabelPM, javax.swing.GroupLayout.PREFERRED_SIZE, 520, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(463, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 2, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(3, 3, 3)
+                .addComponent(jLabelPM, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        getContentPane().add(jPanel1);
+        jPanel1.setBounds(0, 620, 1370, 60);
 
         mTrangChu.setText("Trang chủ");
+
+        jMenuItem2.setText("jMenuItem2");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        mTrangChu.add(jMenuItem2);
+
         mMenu.add(mTrangChu);
 
         mInforUser.setText("Thông tin người dùng");
@@ -117,22 +190,32 @@ public class StudentMain extends javax.swing.JFrame {
         mInforUser.add(mDoiMatKhau);
 
         mThongTinCaNhan.setText("Thông tin cá nhân");
+        mThongTinCaNhan.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mThongTinCaNhanActionPerformed(evt);
+            }
+        });
         mInforUser.add(mThongTinCaNhan);
 
         mMenu.add(mInforUser);
 
-        mSearch.setText("Tra cứu");
+        mSearch.setText("Tool");
 
-        mTimKiemMonHoc.setText("Môn học");
-        mSearch.add(mTimKiemMonHoc);
-
-        mTimKiemSV.setText("Sinh viên");
-        mTimKiemSV.addActionListener(new java.awt.event.ActionListener() {
+        mTimKiemSinhVien.setText("Tìm Kiếm");
+        mTimKiemSinhVien.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                mTimKiemSVActionPerformed(evt);
+                mTimKiemSinhVienActionPerformed(evt);
             }
         });
-        mSearch.add(mTimKiemSV);
+        mSearch.add(mTimKiemSinhVien);
+
+        jMenuItem1.setText("Thêm mới");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        mSearch.add(jMenuItem1);
 
         mMenu.add(mSearch);
 
@@ -145,32 +228,87 @@ public class StudentMain extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void mTimKiemSVActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mTimKiemSVActionPerformed
-        // TODO add your handling code here:
-        pMain.remove(pQL);
-        pMain.add(pSearchStudent);
-        pSearchStudent.setBounds(0, 0,  pMain.getWidth(),pMain.getHeight());
-        this.invalidate();
-        this.validate();
-        repaint();
-    }//GEN-LAST:event_mTimKiemSVActionPerformed
-
     private void mDoiMatKhauActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mDoiMatKhauActionPerformed
         // TODO add your handling code here:
-       frmChangePassWord jChangePassword = new frmChangePassWord();
+        frmChangePassWord jChangePassword = new frmChangePassWord();
         jChangePassword.setVisible(true);
     }//GEN-LAST:event_mDoiMatKhauActionPerformed
 
-    
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+
+        pMain.removeAll();
+        pMain.add(pnThem);
+        pnThem.setBounds(0, 0, pMain.getWidth(), pMain.getHeight());
+        this.invalidate();
+        this.validate();
+        repaint();
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        pMain.removeAll();
+        pMain.add(home);
+        home.setBounds(0, 0, pMain.getWidth(), pMain.getHeight());
+        this.invalidate();
+        this.validate();
+        repaint();
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void mTimKiemSinhVienActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mTimKiemSinhVienActionPerformed
+        pMain.removeAll();
+        pMain.add(pnQLSV);
+        pnQLSV.setBounds(0, 0, pMain.getWidth(), pMain.getHeight());
+        this.invalidate();
+        this.validate();
+        repaint();
+    }//GEN-LAST:event_mTimKiemSinhVienActionPerformed
+
+    private void mThongTinCaNhanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mThongTinCaNhanActionPerformed
+         pMain.removeAll();
+        pMain.add(pInfo);
+        pInfo.setBounds(0, 0, pMain.getWidth(), pMain.getHeight());
+        this.invalidate();
+        this.validate();
+        repaint();
+    }//GEN-LAST:event_mThongTinCaNhanActionPerformed
+    pnThemMoi pnThem = new pnThemMoi();
     frmQuanLy pQL = new frmQuanLy();
     frmSearchStudent pSearchStudent = new frmSearchStudent();
-    private void addPanel(){
-        pMain.add(pQL);
-        pQL.setBounds(0, 0,  pMain.getWidth(),pMain.getHeight());
+    Home1 home = new Home1();
+    panelQuanLySinhVien pnQLSV = new panelQuanLySinhVien();
+    pnThongTinSV pInfo = new pnThongTinSV();
+
+    private void addPanel() {
+        pMain.add(home);
+        home.setBounds(0, 0, pMain.getWidth(), pMain.getHeight());
         this.invalidate();
         this.validate();
         repaint();
     }
+
+    public class InfoThread extends Thread {
+
+        String s = jLabelPM.getText();
+
+        public InfoThread() {
+            start();
+        }
+
+        public void run() {
+            int index = 0;
+            while (true) {
+                index++;
+                jLabelPM.setText(s.substring(0, index < s.length() ? index : s.length()));
+                if (index == s.length() + 20) {
+                    index = 0;
+                }
+                try {
+                    sleep(90);
+                } catch (InterruptedException ex) {
+                }
+            }
+        }
+    }
+
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -207,13 +345,19 @@ public class StudentMain extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabelPM;
     private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JSeparator jSeparator1;
     private javax.swing.JMenuItem mDoiMatKhau;
     private javax.swing.JMenu mInforUser;
     private javax.swing.JMenuBar mMenu;
     private javax.swing.JMenuItem mThongTinCaNhan;
-    private javax.swing.JMenuItem mTimKiemMonHoc;
-    private javax.swing.JMenuItem mTimKiemSV;
+    private javax.swing.JMenuItem mTimKiemSinhVien;
     private javax.swing.JMenu mTrangChu;
     private javax.swing.JPanel pDHBK;
     private javax.swing.JPanel pLogo;
